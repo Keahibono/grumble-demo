aeab6f97faf3183ab2793faec927cfd0870802ed
Te Vallee <astronomemusic@gmail.com>
2015-08-29 12:06:32 -1000

    grumbles aren't displayed due to tweets. Commented timestamps out for now

--- client/templates/school-feed.js
+++ client/templates/school-feed.js
@@ -8,5 +8,13 @@ Template.schoolFeed.helpers({
 Template.schoolFeed.events({
   'click .js-share': function() {
     Overlay.open('shareOverlay', this);
-  }
+  },
+  'click .js-back': function(event) {
+    nextInitiator = 'back';
+    
+    // XXX: set the back transition via Location.back() when IR 1.0 hits
+    history.back();
+    event.stopImmediatePropagation();
+    event.preventDefault();
+  },
 });
